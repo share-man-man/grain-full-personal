f4120ceec59a35bc7df0b884c61d89361da029d7
GrainFull <651487447@qq.com>
2020-03-22 23:48:02 +0800

    修改格式

--- src/views/apps/SmartHouseKeeper/components/Usual.vue
+++ src/views/apps/SmartHouseKeeper/components/Usual.vue
@@ -1,30 +1,30 @@
 <template>
-    <div class="usual">
-        <grain-full-switch v-model="button1" style="margin: 20px"/>
-        <grain-full-switch v-model="button1" style="margin: 20px"/>
-        <grain-full-switch v-model="button1" style="margin: 20px"/>
-        <grain-full-switch v-model="button1" style="margin: 20px"/>
-    </div>
+  <div class="usual">
+    <grain-full-switch v-model="button1" style="margin: 20px" />
+    <grain-full-switch v-model="button1" style="margin: 20px" />
+    <grain-full-switch v-model="button1" style="margin: 20px" />
+    <grain-full-switch v-model="button1" style="margin: 20px" />
+  </div>
 </template>
 
 <script>
-    import GrainFullSwitch from "../../../../components/GrainFull/Neumorphism/GrainFullSwitch";
-    export default {
-        name: "Usual",
-        components: {GrainFullSwitch},
-        data(){
-            return {
-                button1:false
-            }
-        }
-    }
+import GrainFullSwitch from "../../../../components/GrainFull/Neumorphism/GrainFullSwitch";
+export default {
+  name: "Usual",
+  components: { GrainFullSwitch },
+  data() {
+    return {
+      button1: false
+    };
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    .usual{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-    }
+.usual {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+}
 </style>
