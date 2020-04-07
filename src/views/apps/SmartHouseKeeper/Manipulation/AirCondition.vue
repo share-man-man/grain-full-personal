0cd3485970faa8cab3caad316c22297ad0067255
GrainFull <651487447@qq.com>
2020-04-07 16:37:58 +0800

    完成空调控制界面

--- src/views/apps/SmartHouseKeeper/Manipulation/AirCondition.vue
+++ src/views/apps/SmartHouseKeeper/Manipulation/AirCondition.vue
@@ -1,80 +1,105 @@
 <template>
-  <div>
+  <div style="display:flex;flex-direction: column;height: 100%">
     <div class="head">
       <div class="head-back" @click="back">
         <grain-full-icon name="#icon-arrow-left" />
       </div>
       <p class="p">{{ $options.name }}</p>
     </div>
-    <div
-      style="display: flex;justify-content: space-around;align-items: center"
-    >
-      <div class="icon-flag-p" style="width: 90px">
-        A/C is {{ acFlag ? "ON" : "OFF" }}
-      </div>
-      <grain-full-switch v-model="acFlag" mode>
-        <template v-slot:left>
-          OFF
-        </template>
-        <template v-slot:right>
-          ON
-        </template>
-      </grain-full-switch>
-    </div>
-    <div
-      style="display: flex;justify-content: space-around;align-items: center"
-    >
-      <div class="icon-flag-p" style="width: 90px">
-        Sweep Wind
-      </div>
-      <grain-full-switch v-model="windFlag" mode>
-        <template v-slot:left>
-          <grain-full-icon
-            name="#icon-laihui"
-            :class="[windFlag ? 'active' : '']"
-            style="transform: rotate(-90deg)"
-          />
-        </template>
-        <template v-slot:right>
-          <grain-full-icon
-            name="#icon-laihui"
-            :class="[!windFlag ? 'active' : '']"
-          />
-        </template>
-      </grain-full-switch>
-    </div>
-    <div style="display: flex;justify-content: center;align-items: center">
-      <grain-full-circle
-        size="200px"
-        :percent="temperaturePercent"
-        dotted-line-divide-num="15"
+    <div style="flex: 1">
+      <div
+        style="display: flex;justify-content: space-around;align-items: center;"
       >
-        {{Math.floor(temperaturePercent*0.14) + 16}}
-      </grain-full-circle>
-    </div>
-    <div
-      style="display: flex;justify-content: space-around;align-items: center;margin-top: 20px"
-    >
-      <grain-full-circle
-        size="150px"
-        :percent="windPercent"
-        dotted-line-divide-num="4"
-        active-color="#70cdbb"
-        active-light-color="#82dbc7"
-        active-dark-color="#6ac8b5"
+        <div class="icon-flag-p" style="width: 90px">
+          A/C is {{ acFlag ? "ON" : "OFF" }}
+        </div>
+        <grain-full-switch v-model="acFlag" mode>
+          <template v-slot:left>
+            OFF
+          </template>
+          <template v-slot:right>
+            ON
+          </template>
+        </grain-full-switch>
+      </div>
+      <div
+        style="display: flex;justify-content: space-around;align-items: center"
       >
-        {{ Math.floor(windPercent/25)}}
-      </grain-full-circle>
-      <grain-full-circle
-        size="150px"
-        :percent="humidityPercent"
-        dotted-line-divide-num="10"
-        active-color="#408ab2"
-        active-dark-color="#408ab2"
-        active-light-color="#4da0ca"
-      ></grain-full-circle>
+        <div class="icon-flag-p" style="width: 90px">
+          Sweep Wind
+        </div>
+        <grain-full-switch v-model="windFlag" mode>
+          <template v-slot:left>
+            <grain-full-icon
+              name="#icon-laihui"
+              :class="[windFlag ? 'active' : '']"
+              style="transform: rotate(-90deg)"
+            />
+          </template>
+          <template v-slot:right>
+            <grain-full-icon
+              name="#icon-laihui"
+              :class="[!windFlag ? 'active' : '']"
+            />
+          </template>
+        </grain-full-switch>
+      </div>
+      <div style="display: flex;justify-content: center;align-items: center">
+        <grain-full-circle
+          size="180px"
+          :percent="temperaturePercent"
+          dotted-line-divide-num="15"
+        >
+          {{ Math.floor(temperaturePercent * 0.14) + 16 }} &#8451;
+        </grain-full-circle>
+      </div>
+      <div
+        style="display: flex;justify-content: space-around;align-items: center;margin-top: 20px"
+      >
+        <div>
+          <grain-full-circle
+            size="130px"
+            :percent="windPercent"
+            dotted-line-divide-num="4"
+            active-color="#70cdbb"
+            active-light-color="#82dbc7"
+            active-dark-color="#6ac8b5"
+          >
+            {{ Math.floor(windPercent / 25) }}
+          </grain-full-circle>
+          <p class="p" style="margin-top: 5px;font-size: 12px">Strength</p>
+        </div>
+        <div>
+          <grain-full-circle
+            size="130px"
+            :percent="humidityPercent"
+            dotted-line-divide-num="10"
+            active-color="#408ab2"
+            active-dark-color="#408ab2"
+            active-light-color="#4da0ca"
+          ></grain-full-circle>
+          <p class="p" style="margin-top: 5px;font-size: 12px">Humidity</p>
+        </div>
+      </div>
     </div>
-<!--    <grain-full-button @click="test">测试</grain-full-button>-->
+    <!--    <grain-full-button @click="test">测试</grain-full-button>-->
+    <div class="scroll-panel" style="padding-bottom: 10px">
+      <grain-full-panel
+        v-for="(item, index) in statusList"
+        :key="index"
+        :active="statusActiveIndex === index"
+        @click.native="clickTab(index)"
+        icon="#icon-guizi-"
+        style="margin: 15px 0 15px 30px;padding: 10px"
+      >
+        <grain-full-icon :name="item.icon" />
+        <p style="margin:0 0 0 5px;font-size: 13px;font-weight: 500">
+          {{ item.name }}
+        </p>
+      </grain-full-panel>
+      <!--防止右边margin无效-->
+      <div><div style="height: 1px;width: 10px"></div></div>
+    </div>
   </div>
 </template>
 
@@ -82,14 +107,16 @@
 import GrainFullIcon from "../../../../components/GrainFull/Neumorphism/GrainFullIcon";
 import GrainFullSwitch from "../../../../components/GrainFull/Neumorphism/GrainFullSwitch";
 import GrainFullCircle from "../../../../components/GrainFull/Neumorphism/GrainFullCircle";
-import GrainFullButton from "../../../../components/GrainFull/Neumorphism/GrainFullButton";
+import GrainFullPanel from "../../../../components/GrainFull/Neumorphism/GrainFullPanel";
+// import GrainFullButton from "../../../../components/GrainFull/Neumorphism/GrainFullButton";
 export default {
   name: "AirCondition",
   components: {
-    GrainFullButton,
+    // GrainFullButton,
     GrainFullCircle,
     GrainFullSwitch,
-    GrainFullIcon
+    GrainFullIcon,
+    GrainFullPanel
   },
   data() {
     return {
@@ -97,7 +124,29 @@ export default {
       windFlag: false,
       temperaturePercent: 20,
       windPercent: 25,
-      humidityPercent: 11
+      humidityPercent: 11,
+      statusActiveIndex: -1,
+      statusList: [
+        {
+          name: "Control",
+          active: false,
+          icon: "#icon-outline",
+          data: [40, 75, 27]
+        },
+        {
+          name: "Going Out",
+          active: false,
+          icon: "#icon-qiangfeng",
+          data: [90, 50, 73]
+        },
+        {
+          name: "Sleeping",
+          active: false,
+          icon: "#icon-sleep",
+          data: [50, 100, 65]
+        }
+        // { name: "Napping", active: false, icon: "#icon-chair" }
+      ]
     };
   },
   methods: {
@@ -105,10 +154,20 @@ export default {
       this.$router.back();
     },
     test() {
-      this.temperaturePercent = Math.floor(Math.random() * 100 );
+      this.temperaturePercent = Math.floor(Math.random() * 100);
       this.windPercent = Math.floor(Math.random() * 4) * 25;
       this.humidityPercent = Math.floor(Math.random() * 100);
+    },
+    clickTab(index) {
+      // console.log(index)
+      this.statusActiveIndex = index;
+      this.temperaturePercent = this.statusList[index].data[0];
+      this.windPercent = this.statusList[index].data[1];
+      this.humidityPercent = this.statusList[index].data[2];
     }
+  },
+  mounted() {
+    this.clickTab(0)
   }
 };
 </script>
@@ -153,5 +212,18 @@ export default {
 }
 .active {
   color: $GrainFullMainColor;
+}
+
+.scroll-panel {
+  margin-top: 0px;
+  display: flex;
+  align-items: center;
+  overflow-y: hidden;
+  overflow-x: auto;
+  &::-webkit-scrollbar {
+    display: none;
+    width: 0;
+    background: transparent;
+  }
 }
 </style>
